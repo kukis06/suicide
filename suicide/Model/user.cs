59c6e929984f25381a5ff404018d3a194cca48e7
kukis06 <kukis06@seznam.cz>
2023-03-30 08:22:01 +0200

    tradaa

---
+++ suicide/Model/user.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suicide.Model
+{
+    class user
+    {
+        public int score { get; set; }
+    }
+}
