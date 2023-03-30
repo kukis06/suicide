3e271fc4e59b289c7b2512b82f3f4fde2c2041a5
21ib02_benda <21ib02_benda@U1-05>
2023-03-30 08:02:18 +0200

    Přidejte soubory projektu.

---
+++ suicide/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace suicide
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int counter = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            counter++;
+            pocet.Content = counter.ToString();
+        }
+
+
+    }
+}
