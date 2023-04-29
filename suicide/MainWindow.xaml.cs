dad6b71941fe3c8b7127d66a7ecbf2998d630002
kukis06 <kukis06@seznam.cz>
2023-04-29 12:50:28 +0200

    gejmr

--- suicide/MainWindow.xaml.cs
+++ suicide/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace suicide
     public partial class MainWindow : Window
     {
         int counter = 0;
-        private static object txtData;
+        int prvniupgrade = 1;
 
         public MainWindow()
         {
@@ -34,13 +34,39 @@ namespace suicide
 
         public void Button_Click(object sender, RoutedEventArgs e)
         {
-            counter++;
-            aktivni.Content = counter.ToString();
+            if(prvniupgrade == 1) 
+            {
+                counter++;
+                aktivni.Content = counter.ToString();
+            }
+
+            if(prvniupgrade == 2)
+            {
+                counter++;
+                counter++;
+                aktivni.Content = counter.ToString();
+            }
+        }
+
+        public void minus5counter(object sender, RoutedEventArgs e)
+        {
+            counter--;
+            counter--;
+            counter--;
+            counter--;
+            counter--;
         }
 
         private void click_plus(object sender, RoutedEventArgs e) 
         {
-            
+            if (prvniupgrade == 1 && counter >= 15) 
+            {
+                minus5counter(sender, e);
+                minus5counter(sender, e);
+                minus5counter(sender, e);
+                prvniupgrade++;
+                aktivni.Content = counter.ToString();
+            }
         }
     }
 }
