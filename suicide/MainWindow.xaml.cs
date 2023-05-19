9e3f1874c56de4a38c6fe554bed8e992a292c351
kukis06 <kukis06@seznam.cz>
2023-05-19 23:42:15 +0200

    tramtadadaaa

--- suicide/MainWindow.xaml.cs
+++ suicide/MainWindow.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Navigation;
 using System.Data.SQLite;
 using System.Windows.Shapes;
 using System.Reflection.Metadata;
+using suicide.Model;
+using System.Windows.Threading;
+using System.Diagnostics.Metrics;
 
 namespace suicide
 {
@@ -23,177 +26,70 @@ namespace suicide
     /// </summary>
     public partial class MainWindow : Window
     {
-        int counter = 0;
-        int prvniupgrade = 1;
-        int prvnipassiveupgrade = 1;
+        Int64 counter = 0;
+        int clickPower = 1;
+        int passive = 0;
 
-        int test = 1;
-
-        public void minus5counter(object sender, RoutedEventArgs e)
-        {
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-        }
-        public void minus10counter(object sender, RoutedEventArgs e)
-        {
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-        }
-        public void minus15counter(object sender, RoutedEventArgs e)
-        {
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-        }
-        public void minus20counter(object sender, RoutedEventArgs e)
-        {
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-            counter--;
-        }
         public MainWindow()
         {
             InitializeComponent();
-
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += clickAuto;
+            timer.Start();
         }
+        private void clickAuto(object sender, RoutedEventArgs e)
+        {
+            aktivni.Content = counter.ToString();
+            counter += passive;
+        }
+
 
         public void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (prvniupgrade == 1)
-            {
-                counter++;
-                aktivni.Content = counter.ToString();
-            }
-
-            if (prvniupgrade == 2)
-            {
-                counter++;
-                counter++;
-                aktivni.Content = counter.ToString();
-            }
-
-            if (prvniupgrade == 3)
-            {
-                counter++;
-                counter++;
-                counter++;
-                aktivni.Content = counter.ToString();
-            }
-
-            if (prvniupgrade == 4)
-            {
-                counter++;
-                counter++;
-                counter++;
-                counter++;
-                aktivni.Content = counter.ToString();
-            }
-
-            if (prvniupgrade == 5)
-            {
-                counter++;
-                counter++;
-                counter++;
-                counter++;
-                counter++;
-                aktivni.Content = counter.ToString();
-            }
-
-            //if (prvniupgrade == test)
-            //{
-            //    test = test + 1;
-            //    counter++;
-            //    prvniupgrade++;
-            //    aktivni.Content = counter.ToString();
-            //}
-
-
+            counter += clickPower;
+            aktivni.Content = counter.ToString();
         }
 
 
-        private void click_plus(object sender, RoutedEventArgs e) 
+        private void click_plus(object sender, RoutedEventArgs e)
         {
-            if (prvniupgrade == 1 && counter >= 15) 
-            {
-                minus15counter(sender, e);
-                prvniupgrade++;
-                aktivni.Content = counter.ToString();
-            }
-
-            if (prvniupgrade == 2 && counter >= 30)
-            {
-                minus15counter(sender, e);
-                minus15counter(sender, e);
-                prvniupgrade++;
-                aktivni.Content = counter.ToString();
-            }
-
-            if (prvniupgrade == 3 && counter >= 60)
-            {
-                minus20counter(sender, e);
-                minus20counter(sender, e);
-                minus20counter(sender, e);
-                prvniupgrade++;
-                aktivni.Content = counter.ToString();
-            }
-
-            if (prvniupgrade == 4 && counter >= 120)
-            {
-                minus20counter(sender, e);
-                minus20counter(sender, e);
-                minus20counter(sender, e);
-                minus20counter(sender, e);
-                minus20counter(sender, e);
-                minus20counter(sender, e);
-                prvniupgrade++;
-                aktivni.Content = counter.ToString();
-            }
+            clickPower++;
         }
-
-     
-        private void klikplus1pass(object sender, RoutedEventArgs e)
+        private void clickAuto1(object sender, RoutedEventArgs e)
         {
-            
+            passive++;
         }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void Upgrade(object sender, RoutedEventArgs e)
+        {
+            Upgrady Upgrades = new Upgrady();
+            Upgrades.Show();
+        }
+
+
+        private void endgame(object sender, EventArgs e)
+        {
+            if (counter >= 1000000) //milion
+            {
+                Endgamos endgamos = new Endgamos();
+                endgamos.Show();
+            }
+        }
+
+    }  
 }
---
+++ suicide/Model/Upgrady.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace suicide.Model
+{
+    /// <summary>
+    /// Interakční logika pro Upgrady.xaml
+    /// </summary>
+    public partial class Upgrady : Window
+    {
+        public Upgrady()
+        {
+            InitializeComponent();
+        }
+    }
+}
